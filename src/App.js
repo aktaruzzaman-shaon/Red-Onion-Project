c9573d43a00fd319ee552f3654bf4c0d09ea548a
aktaruzzaman-shaon <96846526+aktaruzzaman-shaon@users.noreply.github.com>
2022-11-24 14:28:50 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Banner from "./Component/Banner/Banner/Banner";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Banner></Banner>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/Component/Banner/Banner/Banner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Banner = () => {
+  return <h2> This is banner </h2>;
+};
+
+export default Banner;
